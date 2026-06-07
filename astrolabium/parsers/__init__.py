838ae1e1777d3b84de622a09690376e95c57c6a6
TheWand3rer <vindemiatrixcollective@gmail.com>
2026-06-07 20:14:47 +0200

    chore: add env example, update pyproject, fix __init__ exports
    - .env.example: document GAIADATA_USER and GAIADATA_PASSWORD
    - pyproject.toml: add test dependencies for online tests
    - __init__.py files: expose new modules (GaiaGCNSParser, wikimedia, etc.)

--- astrolabium/parsers/__init__.py
+++ astrolabium/parsers/__init__.py
@@ -3,4 +3,5 @@ from .gliese_parser import GlieseParser
 from .hip_parser import HipparcosParser
 from .orb6_parser import Orb6Parser
 from .wds_parser import WDSParser
+from .gcns_parser import GaiaGCNSParser
 # from .crossindex import CrossIndexParser
